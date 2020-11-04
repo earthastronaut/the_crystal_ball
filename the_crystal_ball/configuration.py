d04655a5385149ab8ad4b193d8cf5ff2ab181d66
earthastronaut <>
2020-11-04 16:56:53 -0700

    add configuration module

---
+++ the_crystal_ball/configuration.py
@@ -0,0 +1,12 @@
+""" Manage configuration for the model
+"""
+import os
+
+
+class Configuration:
+    """ Class object for handling configuration """
+
+    ROOT_PATH = os.path.abspath(os.path.join(__file__, ".."))
+
+
+config = Configuration()
